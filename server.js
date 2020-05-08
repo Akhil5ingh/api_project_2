468ba47f919919480c53751209aaf878350c11a1
Akhil Singh <47193522+Akhil5ingh@users.noreply.github.com>
2020-05-08 18:30:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,21 +2,41 @@
 // where your node app starts
 
 // init project
-const express = require("express");
-const app = express();
+var express = require('express');
+var app = express();
 
-// we've started you off with Express,
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function(request, response) {
-  response.sendFile(__dirname + "/views/index.html");
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
 });
 
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+
+
+// whoami API endpoint
+app.get("/api/whoami", (req, res) => {
+    
+  res.json({ipaddress: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
+            language: req.headers['accept-language'],
+            software: req.headers['user-agent']});
+
+});
+
+
 // listen for requests :)
-const listener = app.listen(process.env.PORT, function() {
-  console.log("Your app is listening on port " + listener.address().port);
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
 });
